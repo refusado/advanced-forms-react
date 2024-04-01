38182775793fc140e36175b3f91ff80b5084b04b
Refusado <refusado@gmail.com>
2024-04-01 08:06:37 -0300

    creating form + styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ export default {
     './src/**/*.tsx'
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      }
+    },
   },
   plugins: [],
 }
